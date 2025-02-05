fe8224d89cbd650f4092482a7caa89464928cb3e
ZXerniXZ <ZXerniXZ@gmail.com>
2025-02-05 12:06:54 +0100

    commit temp

---
+++ prova.java
@@ -0,0 +1,23 @@
+public class prova {
+
+    public static void main(String[] args) {
+        
+        creaPizza("pomodoro","mozzarella","funghi");
+
+    }
+
+
+    public static void creaPizza(String ingrediente) {
+        System.out.println("pizza 1 ingrediente");
+    }
+    
+    public static void creaPizza(String ingrediente,String ingrediente1) {
+        System.out.println("pizza 2 ingredienti");
+    }
+
+    public static void creaPizza(String ingrediente,String ingrediente1,String ingrediente2) {
+        System.out.println("pizza 3 ingredienti");
+    }
+
+ 
+}
